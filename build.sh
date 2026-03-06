f44440effb7d049795aa7fa447027c8dc47d6974
Talha03creator <moyih50210@gmail.com>
2026-03-06 19:36:14 +0500

    fix: remove Pipfile to restore requirements.txt processing

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Force install dependencies to overcome Vercel caching issues
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
-echo "Dependencies installed successfully."
